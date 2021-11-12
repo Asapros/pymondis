a741683d86aa8f8a60c61fc99ce03a4ceced3335
Asapros <asapros@wp.pl>
2021-11-12 11:57:57 +0100

    Reformat

--- pymondis/exceptions.py
+++ pymondis/exceptions.py
@@ -5,6 +5,8 @@ from typing import Type
 class RevoteError(Exception):
     def __init__(self, category: str):
         super().__init__("Tried to vote for category '{}' second time".format(category))
+
+
 class NoEnumMatch(Exception):
     def __init__(self, enum: Type[Enum], value):
         super().__init__("Found no matching elements in {} for value: {}".format(enum, value))
