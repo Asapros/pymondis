15eda5ea31f1521daab5f7c05cf945adbf264da9
Asapros <asapros@wp.pl>
2021-11-12 12:05:45 +0100

    Więcej testów

--- tests/unit.py
+++ tests/unit.py
@@ -1,13 +1,38 @@
+from asyncio import gather
+from datetime import datetime
 from unittest import IsolatedAsyncioTestCase
 
 
 class TestHTTPClient(IsolatedAsyncioTestCase):
     async def test_imports(self):
         from pymondis import api, client, enums, exceptions, models, util, abstract
-    async def test_camps(self):
+
+    async def test_gets(self):
         from pymondis.client import Client
-        async with Client() as c:
-            print(await c.get_camps())
+        async with Client() as client:
+            await gather(
+                client.get_crew(),
+                client.get_camps()
+            )
+
+    async def test_plebiscite(self):
+        from pymondis.client import Client
+        async with Client() as client:
+            await gather(*[client.get_plebiscite(year) for year in range(2019, datetime.now().year + 1)])
+
+    async def test_galleries(self):
+        from pymondis.enums import Castle
+        from pymondis.client import Client
+        async with Client() as client:
+            await gather(*[client.get_galleries(castle) for castle in Castle])
+
+    async def test_photos(self):
+        from pymondis.models import Gallery
+        from pymondis.client import Client
+        async with Client() as client:
+            gallery = Gallery(1)
+            photos = await gallery.get_photos(client.http)
+
 
 if __name__ == "__main__":
     TestHTTPClient.run()
