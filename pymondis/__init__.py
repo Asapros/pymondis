2f081c2a5ecdb46ff20366dc33cfc173da3004fe
Asapros <asapros@wp.pl>
2021-11-27 14:20:59 +0100

    Client jest w init-cie i można go importować od razu

--- pymondis/__init__.py
+++ pymondis/__init__.py
@@ -1,3 +1,3 @@
 from . import api, client, enums, exceptions, models, util
-
-__all__ = "api", "client", "enums", "exceptions", "models", "util"
+from .client import Client
+__all__ = "api", "client", "enums", "exceptions", "models", "util", "Client"
