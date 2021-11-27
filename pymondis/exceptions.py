f9a488739f1e3b2e11c6338c6b60929f6656bab7
Asapros <asapros@wp.pl>
2021-11-27 14:23:38 +0100

    NotFullyImplementedWarning i zmiana nazwy NoEnumMatchError

--- pymondis/exceptions.py
+++ pymondis/exceptions.py
@@ -2,11 +2,15 @@ from enum import Enum
 from typing import Type
 
 
+class NotFullyImplementedWarning(UserWarning):
+    pass
+
+
 class RevoteError(Exception):
     def __init__(self, category: str):
         super().__init__("Próbowałeś zagłosować na kategorię '{}' drugi raz".format(category))
 
 
-class NoEnumMatch(Exception):
+class NoEnumMatchError(Exception):
     def __init__(self, enum: Type[Enum], value):
         super().__init__("Nie znaleziono żadnych pasujących elementów w {} dla wartości: {}".format(enum, value))
