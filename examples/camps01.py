cb39d5534fe37c33215a35d09173463e79b2bcce
Asapros <asapros@wp.pl>
2021-11-27 14:21:21 +0100

    W sumie tylko komentarze

--- examples/camps01.py
+++ examples/camps01.py
@@ -11,9 +11,9 @@ from pymondis.enums import CampLevel
 
 async def main():
     async with Client() as client:
-        camps = await client.get_camps() # Pobiera listę obozów
-        master_camps = filter(lambda camp: camp.level is CampLevel.MASTER, camps) # Filtruje zostawiając tylko masterskie
-        master_camps_sorted = sorted(master_camps, key=lambda camp: camp.promo or camp.price) # Sortuje wg. ceny
+        camps = await client.get_camps()  # Pobiera listę obozów
+        master_camps = filter(lambda camp: camp.level is CampLevel.MASTER, camps)  # Filtruje zostawiając masterskie
+        master_camps_sorted = sorted(master_camps, key=lambda camp: camp.promo or camp.price)  # Sortuje wg. ceny
         for camp in master_camps_sorted:
             print(camp)
 
