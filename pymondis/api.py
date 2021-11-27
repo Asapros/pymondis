4bfb62a7bfd0ab3e06d0631ce67085c8445b10a8
Asapros <asapros@wp.pl>
2021-11-27 14:22:59 +0100

    teraz timeout i baseurl są w konstruktorze

--- pymondis/api.py
+++ pymondis/api.py
@@ -3,18 +3,32 @@ from typing import List, Dict
 
 from httpx import AsyncClient
 
-
 from pymondis.util import default_backoff
 
 
 class HTTPClient(AsyncClient):
-    BASE_URL: str = "https://quatromondisapi.azurewebsites.net/api"
-    TIMEOUT: int | None = None
+    def __init__(
+            self,
+            timeout: float | None = None,
+            *,
+            base_url: str = "https://quatromondisapi.azurewebsites.net/api"
+    ):
+        super().__init__(timeout=timeout)
+        self.base: str = base_url
 
     @default_backoff
-    async def get_resource(self, url: str, cache_time: datetime | None = None, cache_content: bytes | None = None) -> bytes:
-        headers = {"If-Modified-Since": cache_time.strftime("%a, %d %b %Y %H:%M:%S GMT")} if cache_time is not None else {}
-        response = await self.get(url, headers=headers, timeout=self.TIMEOUT)
+    async def get_resource(
+            self,
+            url: str,
+            cache_time: datetime | None = None,
+            cache_content: bytes | None = None
+    ) -> bytes:
+        headers = {
+            "If-Modified-Since": cache_time.strftime("%a, %d %b %Y %H:%M:%S GMT")} if cache_time is not None else {}
+        response = await self.get(
+            url,
+            headers=headers
+        )
         if response.status_code == 304:
             return cache_content
         response.raise_for_status()
@@ -22,74 +36,108 @@ class HTTPClient(AsyncClient):
 
     @default_backoff
     async def get_camps(self) -> List[dict]:
-        response = await self.get(self.BASE_URL + "/Camps", headers={"Accept": "application/json"}, timeout=self.TIMEOUT)
+        response = await self.get(
+            self.base + "/Camps",
+            headers={"Accept": "application/json"}
+        )
         response.raise_for_status()
         return response.json()
 
     @default_backoff
-    async def post_inauguration(self, reservation_model: dict):
-        response = await self.post(self.BASE_URL + "/Events/Inauguration", json=reservation_model, timeout=self.TIMEOUT)
+    async def post_events_inauguration(self, reservation_model: dict):
+        response = await self.post(
+            self.base + "/Events/Inauguration",
+            json=reservation_model
+        )
         response.raise_for_status()
 
     @default_backoff
-    async def get_galleries(self, castle: str) -> List[Dict[str, str | int | bool]]:
-        response = await self.get(self.BASE_URL + "/Images/Galeries/Castle/{}".format(castle), headers={"Accept": "application/json"}, timeout=self.TIMEOUT)  # Galeries
-        response.raise_for_status()
-        return response.json()
-
-    @default_backoff
-    async def get_gallery(self, gallery_id: int) -> List[Dict[str, str]]:
-        response = await self.get(self.BASE_URL + "/Images/Galeries/{}".format(gallery_id), headers={"Accept": "application/json"}, timeout=self.TIMEOUT)  # Znowu 'Galeries'
-        response.raise_for_status()
-        return response.json()
-
-    @default_backoff
-    async def post_fwb(self, purchaser: dict):
-        response = await self.post(self.BASE_URL + "/Orders/FourWorldsBeginning", json=purchaser, timeout=self.TIMEOUT)
-        response.raise_for_status()
-
-    @default_backoff
-    async def post_survey(self, survey_hash: str, result: dict):
-        response = await self.post(self.BASE_URL + "/ParentsZone/Survey/{}".format(survey_hash), json=result, timeout=self.TIMEOUT)
-        response.raise_for_status()
-
-    @default_backoff
-    async def get_crew(self) -> List[dict]:
-        response = await self.get(self.BASE_URL + "/ParentsZone/Crew", headers={"Accept":"application/json"}, timeout=self.TIMEOUT)
+    async def get_images_galleries_castle(self, castle: str) -> List[Dict[str, str | int | bool]]:
+        response = await self.get(
+            self.base + "/Images/Galeries/Castle/{}".format(castle),  # 'Galeries' XD
+            headers={"Accept": "application/json"})
         response.raise_for_status()
         return response.json()
 
     @default_backoff
-    async def post_apply(self):
+    async def get_images_galleries(self, gallery_id: int) -> List[Dict[str, str]]:
+        response = await self.get(
+            self.base + "/Images/Galeries/{}".format(gallery_id),  # Znowu 'Galeries'
+            headers={"Accept": "application/json"})
+        response.raise_for_status()
+        return response.json()
+
+    @default_backoff
+    async def post_orders_four_worlds_beginning(self, purchaser: dict):
+        response = await self.post(
+            self.base + "/Orders/FourWorldsBeginning",
+            json=purchaser
+        )
+        response.raise_for_status()
+
+    @default_backoff
+    async def post_parents_zone_survey(self, survey_hash: str, result: dict):
+        response = await self.post(
+            self.base + "/ParentsZone/Survey/{}".format(survey_hash),
+            json=result
+        )
+        response.raise_for_status()
+
+    @default_backoff
+    async def get_parents_zone_crew(self) -> List[dict]:
+        response = await self.get(
+            self.base + "/ParentsZone/Crew",
+            headers={"Accept": "application/json"}
+        )
+        response.raise_for_status()
+        return response.json()
+
+    @default_backoff
+    async def post_parents_zone_apply(self):
         raise NotImplementedError(
-            "Żeby używać tej metody fajnie by było gdybym wiedział jak dokładnie są przesyłane dane"
+            "Nie mogę aktualnie zaimplementować tej metody, bo nie wiem jak są wysyłane dane"
             "Jeśli jest ci potrzebna możesz otworzyć nowy issue: https://github.com/Asapros/pymondis/issues"
         )
         # Dane najprawdopodobniej są wysyłane jako form, ale nie ma tego w swagger-ze, a ja jestem borowikiem w
         # javascript-a i nie czaje o co chodzi, dodajcie do dokumentacji pls
 
     @default_backoff
-    async def post_subscribe(self, reservation_model: dict) -> List[str]:
-        response = await self.post(self.BASE_URL + "/Reservations/Subscribe", json=reservation_model, headers={"Accept": "application/json"}, timeout=self.TIMEOUT)
+    async def post_reservations_subscribe(self, reservation_model: dict) -> List[str]:
+        response = await self.post(
+            self.base + "/Reservations/Subscribe",
+            json=reservation_model,
+            headers={"Accept": "application/json"}
+        )
         response.raise_for_status()
         return response.json()
 
     @default_backoff
-    async def post_manage(self, pri: dict) -> Dict[str, str | bool]:
-        response = await self.post(self.BASE_URL + "/Reservations/Manage", json=pri, headers={"Accept": "application/json"}, timeout=self.TIMEOUT)
+    async def post_reservations_manage(self, pri: dict) -> Dict[str, str | bool]:
+        response = await self.post(
+            self.base + "/Reservations/Manage",
+            json=pri,
+            headers={"Accept": "application/json"}
+        )
         response.raise_for_status()
         return response.json()
 
     @default_backoff
     async def patch_vote(self, category: str, name: str):
-        response = await self.patch(self.BASE_URL + "/Vote/{}/{}".format(category, name), timeout=self.TIMEOUT)
+        response = await self.patch(  # A mnie dalej zastanawia czemu tu patch jest, a nie post...
+            self.base + "/Vote/{}/{}".format(category, name)
+        )
         response.raise_for_status()
 
     @default_backoff
-    async def get_plebiscite(self, year: int) -> List[Dict[str, str | int | bool]]:
-        response = await self.get(self.BASE_URL + "/Vote/plebiscite/{}".format(year), headers={"Accept": "application/json"}, timeout=self.TIMEOUT)  # Jedyny endpoint gdzie słowo w ścieżce nie się zaczyna dużą literą...
+    async def get_vote_plebiscite(self, year: int) -> List[Dict[str, str | int | bool]]:
+        response = await self.get(
+            self.base + "/Vote/plebiscite/{}".format(year),
+            # Jedyny endpoint gdzie słowo w ścieżce nie się zaczyna dużą literą...
+            headers={"Accept": "application/json"}
+        )
         response.raise_for_status()
         return response.json()
 
     async def __aenter__(self) -> "HTTPClient":  # Type-hinting
-        return await super().__aenter__()
+        await super().__aenter__()
+        return self
