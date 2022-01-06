854eceb913ee6fbbe3c512da2f824cf554b544f1
Asapros <asapros@wp.pl>
2022-01-06 21:47:53 +0100

    Poprawki w przykładach

--- tests/examples/random_vote.py
+++ tests/examples/random_vote.py
@@ -13,6 +13,9 @@ async def main():
     async with Client() as client:
         current_year = datetime.now().year
         candidates = await client.get_plebiscite(current_year)  # Pobiera kandydatów tegorocznego plebiscytu
+        if not candidates:
+            print("Nie ma jeszcze żadnych kandydatów tego roku.")
+            return
         not_voted = list(
             filter(lambda candidate: not candidate.voted, candidates)
         )  # Filtruje kandydatów kategorii, na którą już dzisiaj głosowałeś
